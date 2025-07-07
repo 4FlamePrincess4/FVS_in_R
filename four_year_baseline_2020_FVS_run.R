16eae9cc14fe144e7d93658db5a49219d059e320
4FlamePrincess4 <laurelsindewald@gmail.com>
2025-07-07 09:43:49 -0600

    Add files via upload
    This code creates input files and runs a four-year FVS simulation with no treatments or fire.

---
+++ four_year_baseline_2020_FVS_run.R
@@ -0,0 +1,201 @@
+library(tidyverse) #For data manipulation
+library(RSQLite) #For working with sqlite databases
+library(terra) #For raster and vector geospatial operations
+library(data.table) #Faster data format, works with default R dataframe functions
+library(foreign) #For reading in .dbf files
+library(furrr) #For parallelization
+library(withr) #For parallelization - used to temporarily change the working directory for each worker
+
+#Install the rFVS package
+install.packages("remotes")
+remotes::install_github("SilviaTerra/rFVS")
+library(rFVS)
+
+setwd("D:/WFSETP/Scenario_Development/Baseline_2020_FVS")
+wd <- getwd()
+
+#Set the FVS executable folder
+fvs_bin = "C:/FVS/FVSSoftware/FVSbin"
+
+#---Set location of TMFM sqlite databases and read in data----------------------
+TMFM2020_dir_path <- "D:/WFSETP/Scenario_Development/Chris_FVS_Runs_KCP_Effects/TMFM_2020_OkaWen_Databases"
+
+okawen_dbs <- list.files(TMFM2020_dir_path,
+                         full.names=TRUE)
+#We only need to run this for the East Cascades variant
+EC_dbs <- okawen_dbs[1]
+
+#Name the FVS run
+run_name <- str_c("baseline_2020_FVS_run",
+                  strftime(Sys.Date(), "%d%b%y"),
+                  "_", strftime(Sys.time(), "%H%M"))
+
+# Create a dir for this run.
+RunDirectory <- str_c(wd, "/", run_name)
+dir.create(RunDirectory)
+
+createInputFile_no_trt <- function(stand, managementID, outputDatabase, inputDatabase) {
+  paste0(
+    'STDIDENT\n', stand, '\n',
+    'STANDCN\n', stand, '\n',
+    'MGMTID\n', managementID, '\n',
+    'NUMCYCLE       4\n',
+    'TIMEINT         0        1\n',
+    'SCREEN\n',
+    'DataBase\n',
+    'DSNout\n', outputDatabase, '\n',
+    'SUMMARY\n',
+    'COMPUTDB\n',
+    'CALBSTDB\n',
+    
+    # ---- Report Database Output ----
+    'BURNREDB\n',
+    'CARBREDB\n',
+    'DWDCVDB\n',
+    'FUELREDB\n',
+    'FUELSOUT\n',
+    'MORTREDB\n',
+    'POTFIRDB\n',
+    'SNAGOUDB\n',
+    'SNAGSUDB\n',
+    'STRCLSDB          2\n',
+    'CutLiDB           2         2\n',
+    'TreeLiDB          2         2\n',
+    'End\n',
+    
+    # ---- Input DB Queries ----
+    'DATABASE\n',
+    'DSNIN\n', inputDatabase, '\n',
+    'StandSQL\n',
+    "SELECT * FROM FVS_StandInit\n",
+    "WHERE  Stand_ID  = '%stand_cn%'\n",
+    'EndSQL\n',
+    'DSNIN\n', inputDatabase, '\n',
+    'TreeSQL\n',
+    'SELECT * FROM FVS_TreeInit\n',
+    'WHERE Stand_ID = (SELECT Stand_ID FROM FVS_StandInit\n',
+    "WHERE Stand_ID = '%stand_cn%')\n",
+    'EndSQL\n',
+    'END\n',
+    
+    # ---- COMPUTE block with scenario inputs ----
+    'COMPUTE            0\n',
+    'SEV_FL = POTFLEN(1)\n',
+    'MOD_FL = POTFLEN(2)\n',
+    'scenario = 1\n',
+    'CC = acancov\n',
+    'FML = fuelmods(1,1)\n',
+    'CHT = ATOPHT\n',
+    'CBH = crbaseht\n',
+    'CBD = crbulkdn\n',
+    'YR = year\n',
+    'END\n',
+    
+    # ---- Fire Reports ----
+    'FMIN\n',
+    'BURNREPT\n',
+    'CanFProf\n',
+    'CarbRept\n',
+    'DWDCvOut\n',
+    'FUELREPT\n',
+    'FUELOUT\n',
+    'MORTREPT\n',
+    'POTFIRE\n',
+    'SNAGOUT\n',
+    'SNAGSUM\n',
+    'FIRECALC           0         1         2\n',
+    'END\n',
+    
+    # ---- Tree outputs and classification ----
+    'TreeList        1\n',
+    'TreeList        2\n',
+    'TreeList        3\n',
+    'CALBSTAT\n',
+    'CutList\n',
+    'STRCLASS           1     30.00        5.       16.     20.00       50.     35.00\n',
+    
+    'Process\n\n'
+  )
+}
+
+write_keywords_no_trt <- function(database_path){
+  #Connect to the database and extract the FVS_STANDINIT table
+  con <- dbConnect(SQLite(), database_path)
+  standinit <- dbReadTable(con, "FVS_STANDINIT")
+  dbDisconnect(con)
+  
+  #Create the stand list - fields should be Stand_ID, Variant, and kcp
+  standlist <- data.table::data.table(Stand_ID = standinit$Stand_ID,
+                                      Variant = standinit$Variant)
+  #Create a key label 
+  treat_key <- "Baseline_NoTrt"
+  
+  #Character vector to store the key file text before appending STOP
+  key_text_all <- character()
+  
+  for(j in seq_len(nrow(standinit))){
+    key_text_all <- c(
+      #Create keywords for each unique combination of kcp & variant
+      #Append to the character vector
+      createInputFile_no_trt(stand = standlist$Stand_ID, 
+                             managementID = standlist$Stand_ID, 
+                             outputDatabase = paste0("./outputs/FL0_EC.db"), 
+                             inputDatabase = database_path)
+    )
+  }
+  
+  #Append STOP
+  key_text_all <- c(key_text_all, "STOP\n")
+  
+  # Write output
+  key_file <- file.path(RunDirectory, paste0(treat_key, ".key"))
+  in_file <- file.path(RunDirectory, paste0(treat_key, ".in"))
+  
+  writeLines(key_text_all, key_file)
+  
+  fvs_in <- paste0(
+    treat_key, ".key\n",
+    treat_key, ".fvs\n",
+    treat_key, ".out\n",
+    treat_key, ".trl\n",
+    treat_key, ".sum\n"
+  )
+  
+  writeLines(fvs_in, in_file)
+} #End function to write key and in files
+
+#Write the .key files using the above functions
+write_keywords_no_trt(EC_dbs)
+
+#Make an outputs directory
+dir.create(paste0(RunDirectory, "/outputs"))
+
+runFVS <- function(variant, RunDirectory, fvs_bin) {
+  withr::with_dir(RunDirectory, {
+    # Load the variant DLL
+    rFVS::fvsLoad(
+      fvsProgram = paste0("FVS", variant, ".dll"),
+      bin = fvs_bin
+    )
+    
+    # Set the command line (keyword file)
+    print("--keywordfile=Baseline_NoTrt.key")
+    rFVS::fvsSetCmdLine(
+      cl = "--keywordfile=Baseline_NoTrt.key",
+      PACKAGE = paste0("FVS", variant)
+    )
+    
+    # Run FVS until done — IMPORTANT: pass PACKAGE here too!
+    retCode <- 0
+    while (retCode == 0) {
+      retCode <- rFVS::fvsRun(PACKAGE = paste0("FVS", variant))
+    }
+  })
+  # Explicitly return useful info:
+  list(
+    variant = variant,
+    status = retCode
+  )
+}
+
+runFVS("ec", RunDirectory, fvs_bin)
