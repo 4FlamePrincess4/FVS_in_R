6b14c602b20aa269a798d13ef52b7b2d3c18cff7
4FlamePrincess4 <laurelsindewald@gmail.com>
2025-07-08 09:11:05 -0600

    Update three_year_baseline2020_plus_fire_FVS_run.R

--- three_year_baseline2020_plus_fire_FVS_run.R
+++ three_year_baseline2020_plus_fire_FVS_run.R
@@ -11,14 +11,14 @@ install.packages("remotes")
 remotes::install_github("SilviaTerra/rFVS")
 library(rFVS)
 
-setwd("D:/WFSETP/Scenario_Development/Baseline_2020_FVS")
+setwd("C:/WFSETP_FVS/Baseline_2020_FVS")
 wd <- getwd()
 
 #Set the FVS executable folder
 fvs_bin = "C:/FVS/FVSSoftware/FVSbin"
 
 #---Set location of TMFM sqlite databases and read in data----------------------
-TMFM2020_dir_path <- "D:/WFSETP/Scenario_Development/Chris_FVS_Runs_KCP_Effects/TMFM_2020_OkaWen_Databases"
+TMFM2020_dir_path <- "C:/WFSETP_FVS/TMFM_2020_OkaWen_Databases"
 
 okawen_dbs <- list.files(TMFM2020_dir_path,
                          full.names=TRUE)
@@ -26,7 +26,7 @@ okawen_dbs <- list.files(TMFM2020_dir_path,
 EC_dbs <- okawen_dbs[1]
 
 #Name the FVS run
-run_name <- str_c("baseline_2020_FVS_run",
+run_name <- str_c("baseline_2020_with_fire_FVS_run",
                   strftime(Sys.Date(), "%d%b%y"),
                   "_", strftime(Sys.time(), "%H%M"))
 
@@ -34,7 +34,28 @@ run_name <- str_c("baseline_2020_FVS_run",
 RunDirectory <- str_c(wd, "/", run_name)
 dir.create(RunDirectory)
 
-createInputFile_no_trt <- function(stand, managementID, outputDatabase, inputDatabase) {
+#Path to FlameAdjust kcp
+fire_kcp <- "C:/WFSETP_FVS/KCPs/simfire_hardcoded.kcp"
+
+#Create a table of FSim fire scenario parameters (flame lengths and associated fuel moisture and weather conditions)
+FSim_scenarios <- data.frame(
+  flame_length = c(1, 3, 5, 7, 10, 20),
+  fm1 = c(8, 7, 6, 5, 4, 3),
+  fm10 = c(8, 7, 6, 5, 4, 4),
+  fm100 = c(10, 9, 8, 7, 6, 5),
+  fm1000 = c(15, 13, 11, 10, 8, 6),
+  fmduff = c(50, 45, 40, 30, 20, 15),
+  fmlwood = c(110, 100, 90, 80, 60, 50),
+  fmlherb = c(110, 100, 90, 70, 40, 40),
+  fire_year = rep(4, 6),
+  wspd_mph = c(2, 4, 4, 6, 7, 10),
+  temp_F = c(85, 85, 85, 90, 90, 90),
+  mortality = rep(1, 6),
+  per_stand_burned = c(70, 80, 90, 90, 100, 100),
+  season = rep(3, 6)
+)
+
+createInputFile_no_trt_fire <- function(stand, managementID, params_row, outputDatabase, areaSpecificKcp, inputDatabase) {
   paste0(
     'STDIDENT\n', stand, '\n',
     'STANDCN\n', stand, '\n',
@@ -59,8 +80,8 @@ createInputFile_no_trt <- function(stand, managementID, outputDatabase, inputDat
     'SNAGOUDB\n',
     'SNAGSUDB\n',
     'STRCLSDB          2\n',
-    'CutLiDB           2         2\n',
-    'TreeLiDB          2         2\n',
+    'CutLiDB           2         2         2\n',
+    'TreeLiDB          2         2         2\n',
     'End\n',
     
     # ---- Input DB Queries ----
@@ -89,6 +110,20 @@ createInputFile_no_trt <- function(stand, managementID, outputDatabase, inputDat
     'CBH = crbaseht\n',
     'CBD = crbulkdn\n',
     'YR = year\n',
+    'FLEN = ', params_row$flame_length, '\n',
+    'FM1 = ', params_row$fm1, '\n',
+    'FM10 = ', params_row$fm10, '\n',
+    'FM100 = ', params_row$fm100, '\n',
+    'FM1000 = ', params_row$fm1000, '\n',
+    'FMDUFF = ', params_row$fmduff, '\n',
+    'FMLWOOD = ', params_row$fmlwood, '\n',
+    'FMLHERB = ', params_row$fmlherb, '\n',
+    'FIREYEAR = ', params_row$fire_year, '\n',
+    'WSPD = ', params_row$wspd_mph, '\n',
+    'TEMP = ', params_row$temp_F, '\n',
+    'FVS_MORT = ', params_row$mortality, '\n',
+    'FVSpBURN = ', params_row$per_stand_burned, '\n',
+    'FVS_SEAS = ', params_row$season, '\n',
     'END\n',
     
     # ---- Fire Reports ----
@@ -103,7 +138,7 @@ createInputFile_no_trt <- function(stand, managementID, outputDatabase, inputDat
     'POTFIRE\n',
     'SNAGOUT\n',
     'SNAGSUM\n',
-    'FIRECALC           0         1         2\n',
+    'FIRECALC           3\n',
     'END\n',
     
     # ---- Tree outputs and classification ----
@@ -114,63 +149,91 @@ createInputFile_no_trt <- function(stand, managementID, outputDatabase, inputDat
     'CutList\n',
     'STRCLASS           1     30.00        5.       16.     20.00       50.     35.00\n',
     
+    # # ---- CYCLE block to apply fire .kcp in year 4 (cycle 3) ----
+    'OPEN              81\n',
+    fire_kcp, '\n',
+    'ADDFILE           81\n',
+    'CLOSE             81\n',
+    'END\n',
+    
     'Process\n\n'
   )
 }
 
-write_keywords_no_trt <- function(database_path){
-  #Connect to the database and extract the FVS_STANDINIT table
-  con <- dbConnect(SQLite(), database_path)
-  standinit <- dbReadTable(con, "FVS_STANDINIT")
-  dbDisconnect(con)
+write_keywords_parallel <- function(database_path, FSim_scenarios, kcp_path) {
+  # Load stand data once
+  con <- DBI::dbConnect(RSQLite::SQLite(), database_path)
+  standinit <- DBI::dbReadTable(con, "FVS_STANDINIT")
+  DBI::dbDisconnect(con)
   
-  #Create the stand list - fields should be Stand_ID, Variant, and kcp
-  standlist <- data.table::data.table(Stand_ID = standinit$Stand_ID,
-                                      Variant = standinit$Variant)
-  #Create a key label 
-  treat_key <- "Baseline_NoTrt"
-  
-  #Character vector to store the key file text before appending STOP
-  key_text_all <- character()
-  
-  for(j in seq_len(nrow(standinit))){
-    key_text_all <- c(
-      #Create keywords for each unique combination of kcp & variant
-      #Append to the character vector
-      createInputFile_no_trt(stand = standlist$Stand_ID, 
-                             managementID = standlist$Stand_ID, 
-                             outputDatabase = paste0("./outputs/FL0_EC.db"), 
-                             inputDatabase = database_path)
+  # Create scenario-specific .key and .in files in parallel
+  future_pmap(FSim_scenarios, function(...){
+    inputs <- list(...)
+    
+    fl_label <- paste0("FL", inputs$flame_length)
+    scenario_id <- paste0("Scenario_", fl_label)
+    
+    key_text_all <- character()
+    
+    for (j in seq_len(nrow(standinit))) {
+      stand <- standinit$Stand_ID[j]
+      variant <- standinit$Variant[j]
+      key_name <- paste0(fl_label, "_", variant)
+      out_db <- paste0("./outputs/", key_name, ".db")
+      
+      key_text_all <- c(
+        key_text_all,
+        createInputFile_no_trt_fire(
+          stand = stand,
+          managementID = key_name,
+          params_row = inputs,
+          outputDatabase = out_db,
+          areaSpecificKcp = kcp_path,
+          inputDatabase = database_path
+        )
+      )
+    }
+    
+    # Append STOP
+    key_text_all <- c(key_text_all, "STOP\n")
+    
+    # Write output
+    key_file <- file.path(RunDirectory, paste0(scenario_id, ".key"))
+    in_file <- file.path(RunDirectory, paste0(scenario_id, ".in"))
+    
+    writeLines(key_text_all, key_file)
+    
+    fvs_in <- paste0(
+      scenario_id, ".key\n",
+      scenario_id, ".fvs\n",
+      scenario_id, ".out\n",
+      scenario_id, ".trl\n",
+      scenario_id, ".sum\n"
     )
-  }
-  
-  #Append STOP
-  key_text_all <- c(key_text_all, "STOP\n")
-  
-  # Write output
-  key_file <- file.path(RunDirectory, paste0(treat_key, ".key"))
-  in_file <- file.path(RunDirectory, paste0(treat_key, ".in"))
-  
-  writeLines(key_text_all, key_file)
-  
-  fvs_in <- paste0(
-    treat_key, ".key\n",
-    treat_key, ".fvs\n",
-    treat_key, ".out\n",
-    treat_key, ".trl\n",
-    treat_key, ".sum\n"
-  )
-  
-  writeLines(fvs_in, in_file)
-} #End function to write key and in files
+    
+    writeLines(fvs_in, in_file)
+  })
+}
 
-#Write the .key files using the above functions
-write_keywords_no_trt(EC_dbs)
+#Write the scenario .key files in parallel
+#plan(multisession, workers = parallel::detectCores() - 1) 
+plan(multisession, workers = 12)
+write_keywords_parallel(EC_dbs, FSim_scenarios, fire_kcp)
+#Reset parallel backend
+plan(sequential)
+
+#Create the data frame of run scenario parameters to parallelize over
+#The number of rows is the number of FVS runs you're doing
+variants <- "ec"
+kcps <- "simfire_hardcoded"
+flame_lengths <- c(1, 3, 5, 7, 10, 20)
+runs <- expand.grid(variant = variants, kcp = kcps, flame_length = flame_lengths)
+print(runs)
 
 #Make an outputs directory
 dir.create(paste0(RunDirectory, "/outputs"))
 
-runFVS <- function(variant, RunDirectory, fvs_bin) {
+runFVS <- function(variant, kcp, flame_length, RunDirectory, fvs_bin) {
   withr::with_dir(RunDirectory, {
     # Load the variant DLL
     rFVS::fvsLoad(
@@ -179,9 +242,9 @@ runFVS <- function(variant, RunDirectory, fvs_bin) {
     )
     
     # Set the command line (keyword file)
-    print("--keywordfile=Baseline_NoTrt.key")
+    print(paste0("--keywordfile=Scenario_FL", flame_length, ".key"))
     rFVS::fvsSetCmdLine(
-      cl = "--keywordfile=Baseline_NoTrt.key",
+      cl = paste0("--keywordfile=Scenario_FL", flame_length, ".key"),
       PACKAGE = paste0("FVS", variant)
     )
     
@@ -194,8 +257,97 @@ runFVS <- function(variant, RunDirectory, fvs_bin) {
   # Explicitly return useful info:
   list(
     variant = variant,
+    flame_length = flame_length,
     status = retCode
   )
 }
 
-runFVS("ec", RunDirectory, fvs_bin)
+#n_runs <- nrow(runs)
+future::plan(multisession, workers = 30)
+
+#Run FVS in parallel for all combinations
+furrr::future_pmap(
+  list(
+    variant = runs$variant,
+    kcp = runs$kcp,
+    flame_length=runs$flame_length
+  ),
+  runFVS,
+  RunDirectory = RunDirectory,
+  fvs_bin = fvs_bin
+)
+
+plan(sequential)
+
+combine_dbs_general <- function(RunDirectory, rm.files = FALSE, output_db_name = "Full_Set_FVSOut.db") {
+  #List all the .db files in the directory, excluding the final merged output if it already exists
+  list_dbs <- list.files(RunDirectory, pattern = "\\.db$", full.names = TRUE)
+  list_dbs <- list_dbs[!str_detect(list_dbs, output_db_name)] #Removes the output DB if it already exists
+  
+  #Get the union of all table names that exist across all databases
+  all_table_names <- unique(unlist(
+    lapply(list_dbs, function(fn) {
+      con <- dbConnect(SQLite(), fn)
+      tbls <- tryCatch(dbListTables(con), error = function(e) character(0)) #If there's an error, return an empty character vector
+      dbDisconnect(con)
+      tbls
+    })
+  ))
+  
+  #Read each table from each DB (if it exists), and tag it with the DB file name
+  allframes <- lapply(list_dbs, function(fn) {
+    con <- dbConnect(SQLite(), fn)
+    db_label <- basename(fn) #get just the filename
+    
+    #For each table name, try to read it from the current database
+    tbls <- lapply(all_table_names, function(tbl) {
+      df <- tryCatch(dbReadTable(con, tbl), error = function(e) NULL) #Read the table or return NULL
+      if (!is.null(df) && nrow(df) > 0) {
+        df$db <- db_label  #Add a new column with the source database label to track which entries came from which database
+      }
+      if (!is.null(df) && nrow(df) == 0) {
+        message("Note: Table '", tbl, "' in '", db_label, "' is empty.")
+      }
+      
+      df
+    })
+    names(tbls) <- all_table_names #Name the list entries by table name
+    dbDisconnect(con)
+    tbls
+  })
+  
+  #For each table name, combine all the available versions from all databases
+  combined_tables <- list()
+  for (tbl in all_table_names) {
+    tbl_list <- lapply(allframes, function(x) x[[tbl]]) #Get that table from each database
+    tbl_list <- tbl_list[!sapply(tbl_list, is.null)] #Filter out any that were missing (NULL)
+    if (length(tbl_list) > 0) {
+      combined_tables[[tbl]] <- rbindlist(tbl_list, fill = TRUE) #Combine all, filling in NAs for missing columns
+    }
+  }
+  
+  #Write each combined table to a new SQLite database
+  con_out <- dbConnect(SQLite(), file.path(RunDirectory, output_db_name))
+  for (tbl in names(combined_tables)) {
+    df <- combined_tables[[tbl]]
+    if (nrow(df) > 0) {
+      dbWriteTable(con_out, tbl, df) #Create a new table in the output database
+    }
+  }
+  dbDisconnect(con_out)
+  
+  #Optional: clean up source files
+  #Chris had this in his function, but I made the default argument false in case you want to keep the OG databases.
+  #This does save space and saves a few seconds of highlighting the original database files to delete.
+  if (rm.files) {
+    file.remove(list_dbs)
+  }
+  
+  #Memory cleanup
+  rm(allframes, combined_tables)
+  invisible(gc())
+}
+
+database_dir <- paste0(RunDirectory, "/outputs/")
+
+combine_dbs_general(RunDirectory = database_dir, rm.files = FALSE, output_db_name = "Combined_Outputs_All_FLs_txs.db")
